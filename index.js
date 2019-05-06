b510714df6874d7e810aaef018f9586fe288d937
Samuel Oltz <soltz@rapiddg.com>
2019-05-06 10:54:50 -0400

    better code formatting

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-'use strict';
 const path = require('path');
 const nock = require('nock');
 
 class ServerlessPlugin {
-  constructor(serverless, options) {
+  constructor(serverless) {
     this.serverless = serverless;
-    this.options = (serverless.service.custom || {})["serverless-offline-http-mock"];
+    this.options = (serverless.service.custom || {})[
+      'serverless-offline-http-mock'
+    ];
 
     this.hooks = {
-      "before:offline:start:init": this.startHandler.bind(this),
-      "before:appsync-offline:start:startHandler": this.startHandler.bind(this),
+      'before:offline:start:init': this.startHandler.bind(this),
+      'before:appsync-offline:start:startHandler': this.startHandler.bind(this),
     };
   }
 
@@ -18,24 +19,26 @@ class ServerlessPlugin {
       return;
     }
 
-    this.options.forEach(mock => {
-      if (mock.mocks.length) {
-        if (mock.endpoint === undefined) {
-          throw new Error('Offline HTTP Mock: No endpoint defined');
-        }
-        if (mock.directory === undefined) {
-          throw new Error('Offline HTTP Mock: No directory defined');
+    this.options.forEach(option => {
+      if (option.mocks.length) {
+        if (option.hostname === undefined) {
+          throw new Error('Offline HTTP Mock: No hostname defined');
         }
       }
 
-      mock.mocks.forEach(mockPath => {
+      option.mocks.forEach(mockPath => {
         this.serverless.cli.log(`Loading HTTP mocks in ${mockPath}`);
-        const fn = require(path.join(this.serverless.config.servicePath, mock.directory, mockPath));
-        fn(nock, mock.endpoint);
+        const file = path.join(
+          this.serverless.config.servicePath,
+          option.directory,
+          mockPath,
+        );
+        // eslint-disable-next-line import/no-dynamic-require
+        const fn = require(file);
+        fn(nock, option.hostname);
       });
     });
   }
-
 }
 
 module.exports = ServerlessPlugin;
